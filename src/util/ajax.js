e7a0740bb6725b8729abcec55dad4ce11e313952
lidongjie <lidongjie@xiaolian365.com>
2018-01-25 18:44:08 +0800

    switch to online server

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -224,8 +224,8 @@ AjaxHandler.ajax = (resource, body, cb, serviceErrorCb, options) => {
   } else {
     // url = 'http://116.62.236.67:5080' + resource
     // url = 'http://10.0.0.4:5080' + resource
-    // url = 'https://api.xiaolian365.com/m' + resource
-    url = 'http://120.78.246.160:2080' + resource
+    url = 'https://api.xiaolian365.com/m' + resource
+    // url = 'http://120.78.246.160:2080' + resource
   }
 
   const token = getToken()
@@ -249,8 +249,8 @@ AjaxHandler.ajax = (resource, body, cb, serviceErrorCb, options) => {
 AjaxHandler.ajaxClient = (resource, body, cb) => {
   // const domain = 'http://116.62.236.67:5081'
   // const domain = 'http://10.0.0.4:5081'
-  // const domain = 'https://api.xiaolian365.com/c'
-  const domain = 'http://120.78.25.22:2080'
+  const domain = 'https://api.xiaolian365.com/c'
+  // const domain = 'http://120.78.25.22:2080'
   AjaxHandler.ajax(resource, body, cb, null, { domain: domain })
 }
 
