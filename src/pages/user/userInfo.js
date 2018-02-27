47f00540af65da30bf51130704d96a3fc11de39c
lidongjie <lidongjie@xiaolian365.com>
2018-02-27 11:22:38 +0800

    delete useless Fragment

--- src/pages/user/userInfo.js
+++ src/pages/user/userInfo.js
@@ -9,7 +9,6 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { changeOrder, changeFund } from '../../actions'
-const Fragment = React.Fragment
 
 const SEX = {
   1: '男',
