fc505a3a0cd668a9abe7f970670a4f2ad7fb220b
lidongjie <lidongjie@xiaolian365.com>
2018-03-07 16:07:26 +0800

    update credits in user/info

--- src/pages/user/userInfo.js
+++ src/pages/user/userInfo.js
@@ -251,15 +251,11 @@ class UserInfo extends React.Component {
               </li>
               <li>
                 <p>校ok迁移金额:</p>
-                <span>{data.originalBalance ? data.originalBalance : 0}</span>
+                <span>
+                  {data.originalBalance ? `¥${data.originalBalance}` : 0}
+                </span>
               </li>
             </Fragment>
-          ) : null}
-          {data.credits !== undefined ? (
-            <li>
-              <p>用户积分:</p>
-              {data.credits}
-            </li>
           ) : null}
           <li>
             <p>手机号:</p>
