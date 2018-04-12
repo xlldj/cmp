8dfaeae88191ea3ed49e398dd56abfcf149c76c8
lidongjie <lidongjie@xiaolian365.com>
2018-04-12 20:28:53 +0800

    fix doorForbid constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,6 +12,7 @@ import TASK from './task'
 import USER from './user'
 import VERSION from './version'
 import HEATER from './heater'
+import DOORFORBID from './doorForbid'
 
 const CONSTANTS = {
   ...BASIC,
@@ -27,7 +28,8 @@ const CONSTANTS = {
   ...STAT,
   ...TASK,
   ...USER,
-  ...VERSION
+  ...VERSION,
+  ...DOORFORBID
 }
 
 export default CONSTANTS
