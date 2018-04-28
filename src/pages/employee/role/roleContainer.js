fb2bc5b48b126ca88447e919a4124595fd3719ae
lidongjie <lidongjie@xiaolian365.com>
2018-04-28 14:06:56 +0800

    update role

--- src/pages/employee/role/roleContainer.js
+++ src/pages/employee/role/roleContainer.js
@@ -1,22 +1,36 @@
 import React from 'react'
-import { Route, Switch} from 'react-router-dom'
-import {asyncComponent} from '../../component/asyncComponent'
+import { Route, Switch } from 'react-router-dom'
+import { asyncComponent } from '../../component/asyncComponent'
 
-const RoleTable = asyncComponent(()=>import(/* webpackChunkName: "roleTable" */ "./roleTable"))
-const RoleInfo = asyncComponent(()=>import(/* webpackChunkName: "roleInfo" */ "./roleInfo"))
+const RoleTable = asyncComponent(() =>
+  import(/* webpackChunkName: "roleTable" */ './roleTable')
+)
+const RoleInfo = asyncComponent(() =>
+  import(/* webpackChunkName: "roleInfo" */ './roleInfo')
+)
 
 class RoleContainer extends React.Component {
-  render () {
+  render() {
     return (
       <div>
         <Switch>
-          <Route path='/employee/role/detail/:id' render={(props) => (<RoleInfo hide={this.props.hide} {...props} />)} />
-          <Route path='/employee/role/add' render={(props) => (<RoleInfo hide={this.props.hide} {...props} />)} />
-          <Route exact path='/employee/role' render={(props) => (<RoleTable hide={this.props.hide} {...props} />)} />
+          <Route
+            path="/employee/role/detail/:id"
+            render={props => <RoleInfo hide={this.props.hide} {...props} />}
+          />
+          <Route
+            path="/employee/role/add"
+            render={props => <RoleInfo hide={this.props.hide} {...props} />}
+          />
+          <Route
+            exact
+            path="/employee/role"
+            render={props => <RoleTable hide={this.props.hide} {...props} />}
+          />
         </Switch>
       </div>
     )
   }
 }
 
-export default RoleContainer 
+export default RoleContainer
