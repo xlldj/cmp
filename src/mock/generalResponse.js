7babbbc0b2a6a3ffac9a48a8b848f01c8fbdc3c2
lidongjie <lidongjie@xiaolian365.com>
2018-05-07 16:23:31 +0800

    修正费率设置中为吹风机时必穿unitPrice的bug；完成富士康员工信息的mock验证

--- src/mock/generalResponse.js
+++ src/mock/generalResponse.js
@@ -3,3 +3,16 @@ export const add_success = {
     id: 2
   }
 }
+
+export const delete_error = {
+  data: {
+    result: false
+  },
+  displayMessage: 'error'
+}
+
+export const delete_ok = {
+  data: {
+    result: true
+  }
+}
